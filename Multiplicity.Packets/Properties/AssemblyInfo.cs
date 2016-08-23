a1e8acba28a6b449621a4d376769dc2b4036c18b
Tyler Watson <tyler@tw.id.au>
2016-08-23 20:26:19 +1000

    AssemblyInfo for nuspec

--- Multiplicity.Packets/Properties/AssemblyInfo.cs
+++ Multiplicity.Packets/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("LibMultiplicity")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Multiplicity")]
+[assembly: AssemblyDescription("Multiplicity is an open-source, strongly-typed packet framework for Terraria")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("tylerjwatson")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("tw")]
+[assembly: AssemblyCopyright("tylerjwatson, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,6 +19,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("1.0.*")]
+[assembly: ComVisible(true)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
