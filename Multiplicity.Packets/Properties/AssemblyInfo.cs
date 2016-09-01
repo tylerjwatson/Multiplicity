d81dce4b71c5977172030418bb138360ae71ca44
Tyler Watson <tyler.watson@ucg.com.au>
2016-09-02 09:14:03 +1000

    [Meta] NuSpec and 1.1.6089.15820 version

--- Multiplicity.Packets/Properties/AssemblyInfo.cs
+++ Multiplicity.Packets/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 [assembly: ComVisible(true)]
 
 // The following attributes are used to specify the signing key for the assembly,
